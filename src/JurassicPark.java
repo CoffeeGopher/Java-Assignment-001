5b2b7d75db38ba0829372bccf1b2cc0142341f98
CoffeeGopher <coffeegopher1@gmail.com>
2022-08-25 20:26:47 -0700

    Added print statements and Javadocs comment to each of the project classes

--- src/JurassicPark.java
+++ src/JurassicPark.java
@@ -1,9 +1,22 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Brennan Duffy
+ *
+ * @since Version 1.0
+ *
+ **/
 public class JurassicPark {
     /**
      * Prints famous Jurasic Park quote to console with only 1 sout statement.
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
-        // Write your program here
+        String ianQuote = """
+                "Life, uh,
+                finds a way."
+                —Ian Malcolm""";
+
+        System.out.println(ianQuote);
     }
 }
